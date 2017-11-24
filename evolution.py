f92f8bb822fa84f9ec17a911be1716f698f90061
Félix Pinard <felix.pinard@insa-lyon.fr>
2017-11-24 11:03:04 +0100

    add file evolution.py

---
+++ evolution.py
@@ -0,0 +1,11 @@
+import simulation as sim
+
+def start_evol():
+    
+def evol_loop(genome,fitness):
+    
+def inversion(genomedir):
+    
+def indel(genomedir):
+    
+def get_fitness(gene_expression, expected_profile):
